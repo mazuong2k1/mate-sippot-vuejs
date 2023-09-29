4d6ad29c36b472e06b1c96322d1cd80f73bef5a8
Nguyen Vu Quang <a1tp.quang@gmail.com>
2023-09-29 21:30:57 +0700

    fix css

--- src/pages/MetaCommunityStandard.vue
+++ src/pages/MetaCommunityStandard.vue
@@ -24,7 +24,7 @@
                 <div data-biolink-block-id="1273" class="col-12 my-2">
                   <a href="#" data-biolink-block-id="1273" target="_blank">
                     <img
-                      src="https://i.ibb.co/f8gzM1z/A-nh-chu-p-Ma-n-hi-nh-2023-09-01-lu-c-00-30-25.png"
+                      src="src\static\uploads\block_images\6e0d547ceea4da64394b1a4fb37ef007.png"
                       class="img-fluid rounded"
                     />
                   </a>
